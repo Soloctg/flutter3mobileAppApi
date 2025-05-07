b489a4431ab886fb7cb198475535a81658e12642
solomon-ctg <solomon.iroegbu@gmail.com>
2025-05-07 14:18:13 +0100

    Creating First CRUD

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::apiResource('categories', CategoryController::class);
