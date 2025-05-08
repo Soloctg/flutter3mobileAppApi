40ea94318e17f4c4e40b44576c7d475ad9f4b56c
solomon-ctg <solomon.iroegbu@gmail.com>
2025-05-08 16:41:10 +0100

    feat: Transactions CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('transactions', TransactionController::class);
